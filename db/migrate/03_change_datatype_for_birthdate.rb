652ee43a6abed25c7eab5c5422ecadaade1e8d77
Victor Aketch <vickeryace@gmail.com>
2023-02-23 21:12:58 +0300

    Completed task

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,7 +1,5 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[6.1]
     def change
-      add_column :students, :grade, :Integer
-      add_column :students, :birthdate, :datetime
-      
+      change_column :students, :birthdate, :datetime
     end
-  end
+end
